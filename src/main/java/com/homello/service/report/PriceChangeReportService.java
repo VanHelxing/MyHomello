bf4d2a561ff94e51c925088af6c5a569ccbfa32f
VanHelxing <18056047377@qq.com>
2016-11-02 17:31:27 +0800

    add PriceChangeReport web

--- src/main/java/com/homello/service/report/PriceChangeReportService.java
+++ src/main/java/com/homello/service/report/PriceChangeReportService.java
@@ -1,10 +1,13 @@
 package com.homello.service.report;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import com.homello.dao.report.PriceChangeReportIDao;
+import com.homello.domian.report.PriceChangeReport;
 
 @Service
 public class PriceChangeReportService {
@@ -13,6 +16,15 @@ public class PriceChangeReportService {
 	private PriceChangeReportIDao priceChangeReportDao;
 	
 	
-	
+	/*
+	 * 获取所有金额明细数据 
+	 * 
+	 * @return List<PriceChangeReport>
+	 */
+	public List<PriceChangeReport> queryAllPriceChangeReport() {
+		
+		List<PriceChangeReport> listPriceChangeReports = priceChangeReportDao.queryAllPriceChangeReport();
+		return listPriceChangeReports;
+	}
 	
 }
--- src/main/java/com/homello/web/report/PriceChangeReportController.java
+++ src/main/java/com/homello/web/report/PriceChangeReportController.java
@@ -1,16 +1,31 @@
 package com.homello.web.report;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.alibaba.fastjson.JSON;
+import com.homello.domian.report.PriceChangeReport;
 import com.homello.service.report.PriceChangeReportService;
 
-@Controller
+@Controller("/priceChangeReport")
 public class PriceChangeReportController {
 
 	@Resource
 	private PriceChangeReportService priceChangeReportService;
 	
 	
+	@RequestMapping("/queryAll")
+	public String queryAllPriceChangeReport(ModelMap modelMap) {
+		
+		List<PriceChangeReport> listParams = priceChangeReportService.queryAllPriceChangeReport();
+		String jsonString = JSON.toJSONString(listParams);
+		modelMap.addAttribute("jsonString", jsonString);
+		
+		return "forum/report/PurchReport/PriceChangeReport";
+	}
 }
